04a912dca1cff809e8ebd7c216efca7f02ad64bb
E.G.D.Dulaji <dithuegodagamage@gmail.com>
2023-10-31 22:33:54 +0530

    Student

---
+++ app/views/student/studentProfileCreationSuccessful.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title><?php echo SITENAME ?></title>
+
+        <!-- External CSS -->
+        <link rel="stylesheet" href="<?php echo URLROOT ?>/css/components/studentProfileCreationSuccessful.css">
+    </head>
+    <body>
+
+    <div class="donor-registration">
+        <div class="image">
+            <img src="images/welcom.jpg" alt="Welcome_Image" id="welcome">
+        </div>
+        <div class="formc">
+            <div class="formce">
+                <img src="images/logo.png" alt="Logo_Image" id="logo">
+            
+                <h1>Thank You!</h1>
+                <span style="color: rgb(146, 141, 141);">Our team will validate your information and contact you via email shortly. <br>Your account will be validated between 2-3 days</span> 
+            </div>
+        </div>
+    </div>
