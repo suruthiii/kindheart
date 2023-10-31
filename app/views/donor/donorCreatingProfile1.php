6b660e8d59628418b7060b74c23b896dc7f1be9e
E.G.D.Dulaji <dithuegodagamage@gmail.com>
2023-10-31 20:51:17 +0530

    donor account creation

---
+++ app/views/donor/donorCreatingProfile1.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <title><?php echo SITENAME ?></title>
+
+        <!-- External CSS -->
+        <link rel="stylesheet" href="<?php echo URLROOT ?>/css/components/donorCreatingProfile1.css">
+    </head>
+    <body>
+
+<div class="container">
+        <div class="image">
+            <img src="images/welcom.jpg" alt="Welcome_Image" id="welcome">
+        </div>
+        <div class="formc">
+            <div class="formce">
+                <img src="images/logo.png" alt="Logo_Image" id="logo">
+            
+                <h1>Creating Profile</h1>
+                <span style="color: rgb(146, 141, 141);">Provide donations for their educational success</span><br><br><br>     
+                <span style="color: rgb(146, 141, 141);">You're a</span>
+                <div class="text">                                          
+                    <button class="choice" id="choice1">Individual</button><br>
+                    <button class="choice" id="choice2">Company</button>
+                </div>
+                <div class="button-next">
+                    <button class="next">Next</button>
+                </div>            
+            </div>       
+        </div>
+    </div>
