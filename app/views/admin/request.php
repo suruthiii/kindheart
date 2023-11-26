8aaf3b048c73a53e099f68664a19b0e18401abbb
Suruthi <suruthi0611@gmail.com>
2023-11-27 03:29:57 +0530

    Added Requests page

--- app/views/admin/request.php
+++ app/views/admin/request.php
@@ -11,8 +11,35 @@
     <section class="section" id="main">
         <div class="container">
             <h3>View Requests</h3>
+            <p>View the list of requests</p>
+            <div class="req-list">
+                <div class="list-title">
+                    <h4>User name</h4>
+                </div>
+                <div class="list-items">
+                    <table>
+                        <!-- Request 1 -->
+                        <tr>
+                            <td class="name">
+                                <h4>A.L.M.Callister</h4>
+                            </td>
+                            <td class="btn">
+                                <button class="rev-btn">Review</button>
+                            </td>
+                        </tr>
 
-            
+                        <!-- Request 2 -->
+                        <tr>
+                            <td class="name">
+                                <h4>A.L.M.Callister</h4>
+                            </td>
+                            <td class="btn">
+                                <button class="rev-btn">Review</button>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
         </div>
     </section>
 </main>
