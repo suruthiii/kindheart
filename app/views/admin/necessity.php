9cb21ef7ca9450b5460b9c0e8c723b95680b6e13
Suruthi <suruthi0611@gmail.com>
2023-12-01 03:26:27 +0530

    Updated Necessity page

--- app/views/admin/necessity.php
+++ app/views/admin/necessity.php
@@ -11,11 +11,83 @@
     <section class="section" id="main">
         <div class="container">
             <h3>Posted Nessicities</h3>
-            <p>Last 30 days</p>
+            <p style="margin-left: 10px;">Last 30 days</p>
             <div class="list">
                 <div class="list-title">
                     <h4>Pending</h4>
                 </div>
+                
+                <div class="card-list">
+                    <a href="">
+                        <div class="card">
+                            <table>
+                                <tr>
+                                    <td width="10%"><img src="<?php echo URLROOT ?>/img/house.png" alt=""></td>
+                                    <td width="50%" class="content">
+                                        <h4>Stationary</h4>
+                                        <p>80 Page CR Book</p>
+                                    </td>
+                                    <td width="30%" class="amount">Rs. 34,000.00</td>
+                                    <td width="10%" class="option">
+                                        <form action="<?php echo URLROOT ?>" method="post" class="delete-form">
+                                            <input type="text" name="name" id="name" hidden value="" />
+                                            <button type="submit" class="delete" onclick="return confirmSubmit();">
+                                                <img src="<?php echo URLROOT ?>/img/trash-solid.svg" alt="">
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div class="card">
+                            <table>
+                                <tr>
+                                    <td width="10%"><img src="<?php echo URLROOT ?>/img/house.png" alt=""></td>
+                                    <td width="50%" class="content">
+                                        <h4>Stationary</h4>
+                                        <p>80 Page CR Book</p>
+                                    </td>
+                                    <td width="30%" class="amount">Rs. 34,000.00</td>
+                                    <td width="10%" class="option">
+                                        <form action="<?php echo URLROOT ?>" method="post" class="delete-form">
+                                            <input type="text" name="name" id="name" hidden value="" />
+                                            <button type="submit" class="delete" onclick="return confirmSubmit();">
+                                                <img src="<?php echo URLROOT ?>/img/trash-solid.svg" alt="">
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div class="card">
+                            <table>
+                                <tr>
+                                    <td width="10%"><img src="<?php echo URLROOT ?>/img/house.png" alt=""></td>
+                                    <td width="50%" class="content">
+                                        <h4>Stationary</h4>
+                                        <p>80 Page CR Book</p>
+                                    </td>
+                                    <td width="30%" class="amount">Rs. 34,000.00</td>
+                                    <td width="10%" class="option">
+                                        <form action="<?php echo URLROOT ?>" method="post" class="delete-form">
+                                            <input type="text" name="name" id="name" hidden value="" />
+                                            <button type="submit" class="delete" onclick="return confirmSubmit();">
+                                                <img src="<?php echo URLROOT ?>/img/trash-solid.svg" alt="">
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </a>
+                </div>
+
                 <!-- <div class="list-items">
                     <a class="item-link" href="">
                         <table>
@@ -70,6 +142,12 @@
         </div>
     </section>
 </main>
+
+<script>
+    function confirmSubmit() {
+        return confirm("Are you sure you want to delete this land?");
+    }
+</script>
 
 <?php require APPROOT.'/views/inc/footer.php'; ?>
 
