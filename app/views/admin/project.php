28fd3a7580e8b14943631fc5b476f52a99b60d3d
Suruthi <suruthi0611@gmail.com>
2023-12-02 17:55:52 +0530

    Added Projects page

--- app/views/admin/project.php
+++ app/views/admin/project.php
@@ -11,8 +11,326 @@
     <section class="section" id="main">
         <div class="container">
             <h3>Posted Projects</h3>
+            <p style="margin-left: 10px">Last 30 days</p>
+            <div class="list">
+                <div class="list-title">
+                    <h4>Ongoing Projects</h4>
+                </div>
+                
+                <div class="card-list">
+                    <a href="">
+                        <div class="card">
+                            <table>
+                                <tr>
+                                    <td width="10%"><img src="<?php echo URLROOT ?>/img/house.png" alt=""></td>
+                                    <td width="50%" class="content">
+                                        <h4>Classroom Renovation</h4>
+                                        <p style="width: 110px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">White paint cans</p>
+                                    </td>
+                                    <td width="30%" class="amount">Rs. 34,000.00</td>
+                                    <td width="10%" class="option">
+                                        <form action="<?php echo URLROOT ?>" method="post" class="delete-form">
+                                            <input type="text" name="name" id="name" hidden value="" />
+                                            <button type="submit" class="delete" onclick="return confirmSubmit();">
+                                                <img src="<?php echo URLROOT ?>/img/trash-solid.svg" alt="">
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </a>
 
-            
+                    <a href="">
+                        <div class="card">
+                            <table>
+                                <tr>
+                                    <td width="10%"><img src="<?php echo URLROOT ?>/img/house.png" alt=""></td>
+                                    <td width="50%" class="content">
+                                        <h4>Classroom Renovation</h4>
+                                        <p style="width: 110px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">White paint cans</p>
+                                    </td>
+                                    <td width="30%" class="amount">Rs. 34,000.00</td>
+                                    <td width="10%" class="option">
+                                        <form action="<?php echo URLROOT ?>" method="post" class="delete-form">
+                                            <input type="text" name="name" id="name" hidden value="" />
+                                            <button type="submit" class="delete" onclick="return confirmSubmit();">
+                                                <img src="<?php echo URLROOT ?>/img/trash-solid.svg" alt="">
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div class="card">
+                            <table>
+                                <tr>
+                                    <td width="10%"><img src="<?php echo URLROOT ?>/img/house.png" alt=""></td>
+                                    <td width="50%" class="content">
+                                        <h4>Classroom Renovation</h4>
+                                        <p style="width: 110px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">White paint cans</p>
+                                    </td>
+                                    <td width="30%" class="amount">Rs. 34,000.00</td>
+                                    <td width="10%" class="option">
+                                        <form action="<?php echo URLROOT ?>" method="post" class="delete-form">
+                                            <input type="text" name="name" id="name" hidden value="" />
+                                            <button type="submit" class="delete" onclick="return confirmSubmit();">
+                                                <img src="<?php echo URLROOT ?>/img/trash-solid.svg" alt="">
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div class="card">
+                            <table>
+                                <tr>
+                                    <td width="10%"><img src="<?php echo URLROOT ?>/img/house.png" alt=""></td>
+                                    <td width="50%" class="content">
+                                        <h4>Classroom Renovation</h4>
+                                        <p style="width: 110px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">White paint cans</p>
+                                    </td>
+                                    <td width="30%" class="amount">Rs. 34,000.00</td>
+                                    <td width="10%" class="option">
+                                        <form action="<?php echo URLROOT ?>" method="post" class="delete-form">
+                                            <input type="text" name="name" id="name" hidden value="" />
+                                            <button type="submit" class="delete" onclick="return confirmSubmit();">
+                                                <img src="<?php echo URLROOT ?>/img/trash-solid.svg" alt="">
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div class="card">
+                            <table>
+                                <tr>
+                                    <td width="10%"><img src="<?php echo URLROOT ?>/img/house.png" alt=""></td>
+                                    <td width="50%" class="content">
+                                        <h4>Classroom Renovation</h4>
+                                        <p style="width: 110px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">White paint cans</p>
+                                    </td>
+                                    <td width="30%" class="amount">Rs. 34,000.00</td>
+                                    <td width="10%" class="option">
+                                        <form action="<?php echo URLROOT ?>" method="post" class="delete-form">
+                                            <input type="text" name="name" id="name" hidden value="" />
+                                            <button type="submit" class="delete" onclick="return confirmSubmit();">
+                                                <img src="<?php echo URLROOT ?>/img/trash-solid.svg" alt="">
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div class="card">
+                            <table>
+                                <tr>
+                                    <td width="10%"><img src="<?php echo URLROOT ?>/img/house.png" alt=""></td>
+                                    <td width="50%" class="content">
+                                        <h4>Classroom Renovation</h4>
+                                        <p style="width: 110px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">White paint cans</p>
+                                    </td>
+                                    <td width="30%" class="amount">Rs. 34,000.00</td>
+                                    <td width="10%" class="option">
+                                        <form action="<?php echo URLROOT ?>" method="post" class="delete-form">
+                                            <input type="text" name="name" id="name" hidden value="" />
+                                            <button type="submit" class="delete" onclick="return confirmSubmit();">
+                                                <img src="<?php echo URLROOT ?>/img/trash-solid.svg" alt="">
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <div class="list">
+                <div class="list-title">
+                    <h4>Completed</h4>
+                </div>
+                
+                <div class="card-list">
+                <a href="">
+                        <div class="card">
+                            <table>
+                                <tr>
+                                    <td width="10%"><img src="<?php echo URLROOT ?>/img/house.png" alt=""></td>
+                                    <td width="50%" class="content">
+                                        <h4>Classroom Renovation</h4>
+                                        <p style="width: 110px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">White paint cans</p>
+                                    </td>
+                                    <td width="30%" class="amount">Rs. 34,000.00</td>
+                                    <td width="10%" class="option">
+                                        <form action="<?php echo URLROOT ?>" method="post" class="delete-form">
+                                            <input type="text" name="name" id="name" hidden value="" />
+                                            <button type="submit" class="delete" onclick="return confirmSubmit();">
+                                                <img src="<?php echo URLROOT ?>/img/trash-solid.svg" alt="">
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div class="card">
+                            <table>
+                                <tr>
+                                    <td width="10%"><img src="<?php echo URLROOT ?>/img/house.png" alt=""></td>
+                                    <td width="50%" class="content">
+                                        <h4>Classroom Renovation</h4>
+                                        <p style="width: 110px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">White paint cans</p>
+                                    </td>
+                                    <td width="30%" class="amount">Rs. 34,000.00</td>
+                                    <td width="10%" class="option">
+                                        <form action="<?php echo URLROOT ?>" method="post" class="delete-form">
+                                            <input type="text" name="name" id="name" hidden value="" />
+                                            <button type="submit" class="delete" onclick="return confirmSubmit();">
+                                                <img src="<?php echo URLROOT ?>/img/trash-solid.svg" alt="">
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div class="card">
+                            <table>
+                                <tr>
+                                    <td width="10%"><img src="<?php echo URLROOT ?>/img/house.png" alt=""></td>
+                                    <td width="50%" class="content">
+                                        <h4>Classroom Renovation</h4>
+                                        <p style="width: 110px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">White paint cans</p>
+                                    </td>
+                                    <td width="30%" class="amount">Rs. 34,000.00</td>
+                                    <td width="10%" class="option">
+                                        <form action="<?php echo URLROOT ?>" method="post" class="delete-form">
+                                            <input type="text" name="name" id="name" hidden value="" />
+                                            <button type="submit" class="delete" onclick="return confirmSubmit();">
+                                                <img src="<?php echo URLROOT ?>/img/trash-solid.svg" alt="">
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div class="card">
+                            <table>
+                                <tr>
+                                    <td width="10%"><img src="<?php echo URLROOT ?>/img/house.png" alt=""></td>
+                                    <td width="50%" class="content">
+                                        <h4>Classroom Renovation</h4>
+                                        <p style="width: 110px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">White paint cans</p>
+                                    </td>
+                                    <td width="30%" class="amount">Rs. 34,000.00</td>
+                                    <td width="10%" class="option">
+                                        <form action="<?php echo URLROOT ?>" method="post" class="delete-form">
+                                            <input type="text" name="name" id="name" hidden value="" />
+                                            <button type="submit" class="delete" onclick="return confirmSubmit();">
+                                                <img src="<?php echo URLROOT ?>/img/trash-solid.svg" alt="">
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div class="card">
+                            <table>
+                                <tr>
+                                    <td width="10%"><img src="<?php echo URLROOT ?>/img/house.png" alt=""></td>
+                                    <td width="50%" class="content">
+                                        <h4>Classroom Renovation</h4>
+                                        <p style="width: 110px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">White paint cans</p>
+                                    </td>
+                                    <td width="30%" class="amount">Rs. 34,000.00</td>
+                                    <td width="10%" class="option">
+                                        <form action="<?php echo URLROOT ?>" method="post" class="delete-form">
+                                            <input type="text" name="name" id="name" hidden value="" />
+                                            <button type="submit" class="delete" onclick="return confirmSubmit();">
+                                                <img src="<?php echo URLROOT ?>/img/trash-solid.svg" alt="">
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div class="card">
+                            <table>
+                                <tr>
+                                    <td width="10%"><img src="<?php echo URLROOT ?>/img/house.png" alt=""></td>
+                                    <td width="50%" class="content">
+                                        <h4>Classroom Renovation</h4>
+                                        <p style="width: 110px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">White paint cans</p>
+                                    </td>
+                                    <td width="30%" class="amount">Rs. 34,000.00</td>
+                                    <td width="10%" class="option">
+                                        <form action="<?php echo URLROOT ?>" method="post" class="delete-form">
+                                            <input type="text" name="name" id="name" hidden value="" />
+                                            <button type="submit" class="delete" onclick="return confirmSubmit();">
+                                                <img src="<?php echo URLROOT ?>/img/trash-solid.svg" alt="">
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <div class="right-content">
+                <div class="right-cards">
+
+                    <!-- Card 1 -->
+                    <div class="right-card">
+                        <div class="title">Posted Projects</div>
+                        <div class="value">58,977</div>
+                    </div>
+
+                    <!-- Card 2 -->
+                    <div class="right-card">
+                        <div class="title">Completed Projects</div>
+                        <div class="value">58,977</div>
+                    </div>
+
+                    <!-- Card 3 -->
+                    <div class="right-card">
+                        <div class="title">Monthly Funds</div>
+                        <div class="value">Rs. 58,977.00</div>
+                    </div>
+
+                    <!-- Card 4 -->
+                    <div class="right-card">
+                        <div class="title">Total Funds</div>
+                        <div class="value">Rs. 58,977.00</div>
+                    </div>
+                </div>
         </div>
     </section>
 </main>
