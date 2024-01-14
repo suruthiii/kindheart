96e32a10d94ebfa913b654c9ef0e1723601f5cd5
suruthiii <suruthi0611@gmail.com>
2024-01-15 03:17:54 +0530

    feat: Admin Registration and View Admins functionality for Super Admin

--- app/controllers/SuperAdmin.php
+++ app/controllers/SuperAdmin.php
@@ -5,6 +5,7 @@ class SuperAdmin extends Controller {
         $this->middleware = new AuthMiddleware();
         // Only super admin is allowed to access superadmin pages
         $this->middleware->checkAccess(['superAdmin']);
+        $this->userModel = $this->model('UserModel');
     }
 
     public function index(){
@@ -14,11 +15,13 @@ class SuperAdmin extends Controller {
         $this->view('super admin/index', $data);
     }
 
-    public function admin(){
+    public function admin($other_data = null){
         $data = [
-            'title' => 'Home page'
+            'title' => 'Home page',
+            'admins' => $this->userModel->viewAdmins()
         ];
-        $this->view('super admin/admin', $data);
+
+        $this->view('super admin/admin', $data, $other_data);
     }
 
     public function necessity() {
@@ -86,10 +89,79 @@ class SuperAdmin extends Controller {
     }
 
     public function createAdmin(){
-        $data = [
-            'title' => 'Home page'
-        ];
-        $this->view('super admin/complaint', $data);
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                'username' => trim($_POST['username']),
+                'name' => trim($_POST['name']),
+                'email' => trim($_POST['email']),
+                'password' => trim($_POST['password']),
+                'confirm_password' => trim($_POST['confirm_password']),
+                'user_type' => 'admin',
+                'err' => ''
+            ];
+
+            // Validate username
+            if (empty($data['username'])) {
+                $data['err'] = 'Please enter username';
+            } else {
+                // Check if username exists
+                if ($this->userModel->findUserByUsername($data['username'])) {
+                    $data['err'] = 'Username is already taken';
+                }
+            }
+
+            // Validate name
+            if (empty($data['name'])) {
+                $data['err'] = 'Please enter name';
+            }
+
+            // Validate email
+            if (empty($data['email'])) {
+                $data['err'] = 'Please enter email';
+            } else {
+                // Check if email exists
+                if ($this->userModel->findUserByEmail($data['email'])) {
+                    $data['err'] = 'Email is already taken';
+                }
+            }
+
+            // Validate password
+            if (empty($data['password'])) {
+                $data['err'] = 'Please enter password';
+            } elseif (strlen($data['password']) < 6) {
+                $data['err'] = 'Password must be at least 6 characters';
+            }
+
+            // Validate confirm password
+            if (empty($data['confirm_password'])) {
+                $data['err'] = 'Please confirm password';
+            } else {
+                if ($data['password'] != $data['confirm_password']) {
+                    $data['err'] = 'Passwords do not match';
+                }
+            }
+
+            // die(print_r($data));
+
+            // Make sure errors are empty
+            if (empty($data['err'])) {
+                // Hash password
+                $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+
+                // die(print_r($data));
+                // Register user
+                if ($this->userModel->register($data)) {
+                    redirect('superadmin/admin');
+                } else {
+                    die('Something went wrong');
+                }
+            } else {
+                // Load view with errors
+                $this->admin($data);
+            }
+        }
     }
 
 }    
--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -9,16 +9,52 @@ class UserModel{
     // Register user
     public function register($data){
         // Prepare statement
-        $this->db->query('INSERT INTO user (name, email, password, userType) VALUES (:name, :email, :password, :userType)');
+        $this->db->query('INSERT INTO user (username, email, password, userType) VALUES (:username, :email, :password, :userType)');
 
         // Bind values
-        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':username', $data['username']);
         $this->db->bind(':email', $data['email']);
         $this->db->bind(':password', $data['password']);
         $this->db->bind(':userType', $data['user_type']);
 
         // Execute
-        if ($this->db->execute()){
+        if ($this->db->execute() && $this->updateUserTable($data)){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    // Update user tables
+    public function updateUserTable($data){
+
+        $this->db->query('SELECT userID FROM user WHERE username = :username AND password = :password');
+
+        // Bind values
+        $this->db->bind(':username', $data['username']);
+        $this->db->bind(':password', $data['password']);
+
+        $row = $this->db->single();
+        $id = $row->userID;
+
+        $result = true;
+
+        // Prepare statement
+        if ($data['user_type'] == 'admin'){
+            // Prepare statement
+            $this->db->query('INSERT INTO admin (adminID, adminName) VALUES (:id, :name)');
+
+            // Bind values
+            $this->db->bind(':name', $data['name']);
+            $this->db->bind(':id', $id);
+
+            $result = $this->db->execute();
+        }
+
+
+        // Execute
+        if ($result){
             return true;
         }
         else {
@@ -56,5 +92,30 @@ class UserModel{
         } else {
             return false;
         }
+    }
+
+    // Find user by username
+    public function findUserByUsername($username): bool
+    {
+        $this->db->query('SELECT * FROM user WHERE username = :username');
+        $this->db->bind(':username', $username);
+
+        $row = $this->db->single();
+
+        // Check row
+        if ($this->db->rowCount() > 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // View admins
+    public function viewAdmins(){
+        $this->db->query('SELECT * FROM admin');
+
+        $result =  $this->db->resultSet();
+
+        return $result;
     }
 }
