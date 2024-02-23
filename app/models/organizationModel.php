3cc4e4888b4ccecaad8285c900088587de4dfe8a
sakunika <muthunihansani052@gmail.com>
2024-02-23 15:59:49 +0530

    Add monetary necessity model

--- app/models/organizationModel.php
+++ app/models/organizationModel.php
@@ -11,4 +11,27 @@ class organizationModel{
 
         return $this->db->resultSet();
     }
+
+    public function addmonetarynecessitytodb($data){
+        // if(isset($_SESSION['doneeID'])){
+            $this->db->query('INSERT INTO necessity(name,necessaryType,description,doneeID) 
+            VALUES (:necessityMonetary, :necessityType, :monetarynecessitydes, :doneeID)');
+
+            $this->db->bind(':necessityMonetary', $data['necessityMonetary']);
+            $this->db->bind(':necessityType', $data['necessityType']);
+            $this->db->bind(':monetarynecessitydes', $data['monetarynecessitydes']);
+            $this->db->bind(':doneeID', $_SESSION['user_id']);
+            
+
+            if($this->db->execute()){
+                return true;
+            }else{
+                return false;
+            }
+        // }else{
+        //     return false;
+        // }
+        
+            
+    }
 }
