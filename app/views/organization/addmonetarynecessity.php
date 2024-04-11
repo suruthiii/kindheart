6deb97b2c684968a6f865afdab00be7212b3c67e
sakunika <muthunihansani052@gmail.com>
2024-04-11 13:36:57 +0530

    Finished classifying necessities

--- app/views/organization/addmonetarynecessity.php
+++ app/views/organization/addmonetarynecessity.php
@@ -33,30 +33,34 @@
                             <div class="necessity-first-div">
                                 <label for="necessityMonetary">Necessity</label>
                                 <select id="necessityMonetary" name="necessityMonetary" value="<?php echo isset($data['necessityMonetary']) ? $data['necessityMonetary'] : ''; ?>">
-                                    <option value="EducationalSuppliesandTools" class="tooltip">Educational Supplies and Tools</option>
-                                        <!-- <span class="tooltiptext">
-                                            <ul>
-                                                <li>Pencils</li>
-                                                <li>Pens</li>
-                                                <li>Notebooks</li>
-                                                <li>Textbooks</li>
-                                                <li>Calculators</li>
-                                                <li>Educational software</li>
-                                                <li>Interactive whiteboards</li>
-                                                <li>Microscopes</li>
-                                                <li>Lab equipment</li>
-                                                <li>Robotics kits</li>
-                                                <li>Coding software</li>
-                                                <li>Laptops</li>
-                                                <li>3D printers</li>
-                                            </ul>
-                                        </span> -->
-                                    <option value="ClothingandAccessories">Clothing and Accessories</option>
-                                    <option value="RecreationandSportsEquipment">Recreation and Sports Equipment</option>
-                                    <option value="HealthandWellnessProducts">Health and Wellness Products</option>
-                                    <option value="TransportationandMobility">Transportation and Mobility</option>
-                                    <option value="LiteratureandReadingMaterials">Literature and Reading Materials</option>
-                                    <option value="othernecessitycato">Other</option>
+                                    <!-- &#13 -> use for break the content of title attribute -->
+                                    <option value="EducationalSuppliesandTools" title="Pencils&#13Pens&#13Notebooks&#13Textbooks&#13Calculators&#13Educational software&#13Interactive whiteboards&#13Microscopes&#13Lab equipment&#13Robotics kits&#13Coding software&#13Coding software&#13Laptops&#13 3D printers">
+                                        Educational Supplies and Tools
+                                    </option>
+                                    <!-- &#13 -> use for break the content of title attribute -->
+                                    <option value="ClothingandAccessories" title="School uniforms&#13T-shirts&#13Pants&#13Shoes&#13Backpacks&#13Hats">
+                                        Clothing and Accessories
+                                    </option>
+                                    <!-- &#13 -> use for break the content of title attribute -->
+                                    <option value="RecreationandSportsEquipment" title="Soccer balls&#13Basketballs&#13Gymnastics mats&#13Tennis rackets&#13Bicycles&#13Skateboards">
+                                        Recreation and Sports Equipment
+                                    </option>
+                                    <!-- &#13 -> use for break the content of title attribute -->
+                                    <option value="HealthandWellnessProducts" title="Hygiene products (e.g., soap, toothpaste)&#13Hand sanitizer&#13Yoga mats">
+                                        Health and Wellness Products
+                                    </option>
+                                    <!-- &#13 -> use for break the content of title attribute -->
+                                    <option value="TransportationandMobility" title="Bicycles&#13Wheelchairs&#13Scooters&#13Crutches&#13Walking canes">
+                                        Transportation and Mobility
+                                    </option>
+                                    <!-- &#13 -> use for break the content of title attribute -->
+                                    <option value="LiteratureandReadingMaterials" title="Fiction books&#13Non-fiction books&#13Language learning materials&#13Dictionaries&#13E-readers&#13Audiobooks">
+                                        Literature and Reading Materials
+                                    </option>
+                                    <!-- &#13 -> use for break the content of title attribute -->
+                                    <option value="othernecessitycato">
+                                        Other
+                                    </option>
                                 </select>
                                 <!-- Monetary necessity Error display -->
                                 <span class="form-error-details" style="color: #8E0000; font-family: 'Inter', sans-serif;"><?php echo isset($data['necessityMonetary_err']) ? $data['necessityMonetary_err']: ''; ?></span>
@@ -100,7 +104,7 @@
                         <!-- Description about requested necessity -->
                         <div class="add-necessity-text-area-input-to-oneline">
                             <label for="monetarynecessitydes">Description</label>
-                            <textarea name="monetarynecessitydes" id="monetarynecessitydes" cols="30" rows="10"><?php echo isset($data['monetarynecessitydes']) ? $data['monetarynecessitydes'] : ''; ?></textarea>
+                            <textarea name="monetarynecessitydes" id="monetarynecessitydes" cols="30" rows="10" placeholder="Provide the item that belongs in the necessity category"><?php echo isset($data['monetarynecessitydes']) ? $data['monetarynecessitydes'] : ''; ?></textarea>
                             <!-- Neccessity description error display -->
                             <span class="form-error-details" style="color: #8E0000; font-family: 'Inter', sans-serif;"><?php echo isset($data['monetarynecessitydes_err']) ? $data['monetarynecessitydes_err']: ''; ?></span>
                         </div>
@@ -150,6 +154,7 @@
                     // Add event listener to necessityType select element
                     necessityTypeSelect.addEventListener('change', toggleRecurringFields);
                 });
+
             </script>
             <!-- ---------------------------------------------------------------------------------------------- -->
 
