86eadeb846b06d091d0c109b15f4018a012749de
jayathmaa <78878373+jayathmaa@users.noreply.github.com>
2024-04-21 12:53:06 +0530

    404 page
    404 page added
    applied benefaction amount validation

--- app/views/404.php
+++ app/views/404.php
@@ -1,9 +1,12 @@
 <!-- views/404.php -->
 <?php require APPROOT . '/views/inc/header.php'; ?>
 
-<div class="container">
-    <h1>404 - Page Not Found</h1>
-    <p>The page you are looking for does not exist.</p>
-</div>
+
+
+    <div class="error-container">
+   
+    </div>
+
+
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
