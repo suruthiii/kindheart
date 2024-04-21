bca31be5399d15c404616070a3b33cd5c66f3918
Suruthi <suruthi0611@gmail.com>
2024-04-22 01:55:38 +0530

    fix: Super Admin's View Complaint

--- app/controllers/Complaint.php
+++ app/controllers/Complaint.php
@@ -5,6 +5,7 @@ class Complaint extends Controller {
         // Only admins are allowed to access admin/superadmin pages
         $this->middleware->checkAccess(['superAdmin', 'admin']);
         $this->complaintModel = $this->model('ComplaintModel');
+        $this->userModel = $this->model('UserModel');
     }
 
     public function unassignAdmin() {
@@ -29,17 +30,29 @@ class Complaint extends Controller {
         }    
     }
 
-    public function viewUnassignedComplaint($complaint_ID) {
+    public function viewComplaint() {
+        $complaint_people = $this->complaintModel->getIDs($_GET['complaint_ID']);
+        $complainerID = $complaint_people->complainerID;
+        $complaineeID = $complaint_people->complaineeID;
+        
         $data = [
-            'title' => 'Home Page',
-            // 'complaint_details' => $this->complaintModel->getComplaintDetails($complaint_ID),
-            // 'past_complaints' => $this->complaintModel->getPastComplaints($complaint_ID)
+            'complaint_ID' => $_GET['complaint_ID'],
+            'complainee_name' => $this->complaintModel->getName($complaineeID)->name,
+            'complainer_name' => $this->complaintModel->getName($complainerID)->name,
+            'complaint_description' => $this->complaintModel->getComplaintDetails($_GET['complaint_ID'])->description,
+            'complaint_adminID' => $this->complaintModel->getComplaintDetails($_GET['complaint_ID'])->adminID,
+            'admins' => $this->userModel->viewAdmins()
+            // 'past_complaints' =>  $this->complaintModel->getPastComplaints($complaineeID)
         ];
 
-        $this->view($_SESSION['user_type'].'/complaint/viewUnassignedComplaint', $data);
+        $this->view($_SESSION['user_type'].'/complaint/viewComplaint', $data);
     }
 
-
+    public function banComplainee() {
+        if($this->userModel->banUser($_POST['complainee_ID'])) {
+            redirect('complaint/viewComplaint?complaint_ID='.$data['complaint_ID']);
+       }
+    }
 
 
 }
--- app/models/ComplaintModel.php
+++ app/models/ComplaintModel.php
@@ -15,7 +15,7 @@ class ComplaintModel{
     }
 
     public function getAllAssignedComplaints() {
-        $this->db->query('SELECT c.complaintID, u.username, a.adminName FROM complaint c JOIN user u ON c.complainerID = u.userID JOIN admin a ON c.adminID = a.adminId WHERE c.adminID != 0;');
+        $this->db->query('SELECT c.complaintID, u.username, a.adminName FROM complaint c JOIN user u ON c.complainerID = u.userID JOIN admin a ON c.adminID = a.adminId WHERE c.adminID != 0 AND handlingStatus = 0;');
         
         $result = $this->db->resultSet();
 
@@ -58,8 +58,98 @@ class ComplaintModel{
         }
     }
 
-    public function getPastComplaints($complaint_ID) {
-        // $this->db->query('SELECT c.complaintID, c.description,  FROM ');
+    // Function to get Complainee and Complainer IDs
+    public function getIDs($complaint_ID) {
+        $this->db->query('SELECT complainerID, complaineeID FROM complaint WHERE complaintID = :complaintID;');
+        $this->db->bind(':complaintID', $complaint_ID);
 
+        $ids = $this->db->single();
+
+        return $ids;
+    }
+
+    public function getUserType($user_ID){
+        $this->db->query('SELECT userType FROM user WHERE userID = :userID;');
+        $this->db->bind(':userID', $user_ID);
+
+        $userType = $this->db->single()->userType;
+    
+        if ($userType == 'donor') {
+            $this->db->query('SELECT donorType FROM donor WHERE donorID = :donorID;');
+            $this->db->bind(':donorID', $user_ID);
+
+            $userType =  $this->db->single()->donorType;
+        }
+
+        return $userType;
+    }
+
+    public function getName($user_ID){
+        $userType = $this->getUserType($user_ID);
+
+        if ($userType == 'company'){
+            $this->db->query('SELECT companyName AS name FROM company WHERE companyID = :companyID;');
+            $this->db->bind(':companyID', $user_ID);
+
+            $name = $this->db->single();
+        }
+
+        else if ($userType == 'individual'){
+            $this->db->query('SELECT CONCAT(fName, " ", lName) AS name FROM individual WHERE individualID = :individualID;');
+            $this->db->bind(':individualID', $user_ID);
+
+            $name = $this->db->single();
+        }
+
+        else if ($userType == 'organization'){
+            $this->db->query('SELECT orgName AS name FROM organization WHERE orgID = :orgID;');
+            $this->db->bind(':orgID', $user_ID);
+
+            $name = $this->db->single();
+        }
+
+        else if ($userType == 'student'){
+            $this->db->query('SELECT CONCAT(fName, " ", lName) AS name FROM student WHERE studentID = :studentID;');
+            $this->db->bind(':studentID', $user_ID);
+
+            $name = $this->db->single();
+        }
+
+        return $name;
+    }
+
+    public function getComplaintDetails($complaint_ID){
+        $this->db->query('SELECT adminID, description FROM complaint WHERE complaintID = :complaintID');
+        $this->db->bind(':complaintID', $complaint_ID);
+
+        $details = $this->db->single();
+
+        return $details;
+    }
+
+    public function getComplaineeType($complaint_ID) {
+        $this->db->query('SELECT complaineeType FROM complaint WHERE complaintID = :complaintID');
+        $this->db->bind(':complaintID', $complaint_ID);
+
+        $complaineeType = $this->db->single();
+
+        return $complaineeType;
+    }
+
+    public function getPastComplaintsOthers($complainee_ID){
+        // complaineeType = getComplaineeType($complainee_ID)
+
+        // if complaineeType == 'student' OR complaineeType == 'organization'
+        // SELECT c.complaint, CONCAT(i.fName, " ", i.lName) AS name FROM complaint c JOIN individual i ON c.complainerID = i.individualID WHERE c.complaineeID = :complaineeID
+        // SELECT c.complaint, cn.companyName AS name FROM complaint c JOIN company cn ON c.complainerID = cn.companyID WHERE c.complaineeID = :complaineeID 
+
+        // else if complaineeType == 'donor'
+        // SELECT c.complaint, CONCAT(s.fName, " ", s.lName) AS name FROM complaint c JOIN student s ON c.complainerID = s.studentID WHERE c.complaineeID = :complaineeID
+        // SELECT c.complaint, o.orgName AS name FROM complaint c JOIN organization o ON c.complainerID = o.orgID WHERE c.complaineeID = :complaineeID 
+
+        // $history = $this->db->resultSet();
+
+        // return $history;
     }
 }    
+
