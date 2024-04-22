4f34600fdfe5d496c0d1d1333620e06f044eb9da
Suruthi <suruthi0611@gmail.com>
2024-04-22 16:58:25 +0530

    feat: Admin's View Complainer and Complainee Profiles

---
+++ app/views/admin/complaint/viewDonorIndProfile.php
@@ -0,0 +1,76 @@
+<?php require APPROOT.'/views/inc/header.php'; ?>
+
+<!--  TOP NAVIGATION  -->
+<?php require APPROOT.'/views/inc/components/topnavbar.php'; ?>
+
+<!--  SIDE NAVIGATION  -->
+<?php $section = "complaints"; ?>
+<?php require APPROOT.'/views/inc/components/sidenavbar.php'; ?>
+
+<main class="page-container">
+    <section class="section" id="main">
+        <div class="container">
+            <div class="back-arrow-btn">
+                <a href="<?php echo URLROOT ?>/complaint/viewcomplaint">
+                    <table>
+                        <tr>
+                            <td width="30%"><img class="back-arrow-img" src="<?php echo URLROOT ?>/img/back-arrow.png" alt=""></td>
+                            <td width="70%">Go Back</td>
+                        </tr>
+                    </table>
+                </a>
+            </div>
+
+            <h3 style="margin-top: 25px">View Profile</h3>
+            <p style="margin-left: 10px">View information about the user</p>
+            
+            <div class="necessity-info">
+                <table>
+                    <tr class="necessity-data">
+                            <th width="30%">Donor ID</th>
+                            <td width="70%"><?php print_r($data['details']->donorID); ?></td>
+                        </tr>
+                        <tr class="necessity-data">
+                            <th width="30%">Donor Username</th>
+                            <td width="70%"><?php print_r($data['details']->username); ?></td>
+                        </tr>
+                        <tr class="necessity-data">
+                            <th width="30%">Donor Email</th>
+                            <td width="70%"><?php print_r($data['details']->email); ?></td>
+                        </tr>
+                        <tr class="necessity-data">
+                            <th width="30%">Donor Type</th>
+                            <td width="70%"><?php print_r($data['details']->donorType); ?></td>
+                        </tr>
+                        <tr class="necessity-data">
+                            <th width="30%">First Name</th>
+                            <td width="70%"><?php print_r($data['details']->fName); ?></td>
+                        </tr>
+                        <tr class="necessity-data">
+                            <th width="30%">Last Name</th>
+                            <td width="70%"><?php print_r($data['details']->lName); ?></td>
+                        </tr>
+                        <tr class="necessity-data">
+                            <th width="30%">NIC Number</th>
+                            <td width="70%"><?php print_r($data['details']->nicNo); ?></td>
+                        </tr>
+                        <tr class="necessity-data">
+                            <th width="30%">Gender</th>
+                            <td width="70%"><?php print_r($data['details']->gender); ?></td>
+                        </tr>
+                        <tr class="necessity-data">
+                            <th width="30%">Address</th>
+                            <td width="70%"><?php print_r($data['details']->address); ?></td>
+                        </tr>
+                        <tr class="necessity-data">
+                            <th width="30%">Phone Number</th>
+                            <td width="70%"><?php print_r($data['details']->phoneNumber); ?></td>
+                        </tr>
+                </table>
+            </div>
+
+        </div>
+    </section>
+</main>
+
+<?php require APPROOT.'/views/inc/footer.php'; ?>
---
+++ app/views/admin/complaint/viewOrganizationProfile.php
@@ -0,0 +1,90 @@
+<?php require APPROOT.'/views/inc/header.php'; ?>
+
+<!--  TOP NAVIGATION  -->
+<?php require APPROOT.'/views/inc/components/topnavbar.php'; ?>
+
+<!--  SIDE NAVIGATION  -->
+<?php $section = "complaints"; ?>
+<?php require APPROOT.'/views/inc/components/sidenavbar.php'; ?>
+
+<main class="page-container">
+    <section class="section" id="main">
+        <div class="container">
+            <div class="back-arrow-btn">
+                <a href="<?php echo URLROOT ?>/complaint/viewcomplaint">
+                    <table>
+                        <tr>
+                            <td width="30%"><img class="back-arrow-img" src="<?php echo URLROOT ?>/img/back-arrow.png" alt=""></td>
+                            <td width="70%">Go Back</td>
+                        </tr>
+                    </table>
+                </a>
+            </div>
+
+            <h3 style="margin-top: 25px">View Profile</h3>
+            <p style="margin-left: 10px">View information about the user</p>
+            
+            <div class="necessity-info">
+                <table>
+                <tr class="necessity-data">
+                        <th width="30%">Organization ID</th>
+                        <td width="70%"><?php print_r($data['details']->orgID); ?></td>
+                    </tr>
+                    <tr class="necessity-data">
+                        <th width="30%">Organization Username</th>
+                        <td width="70%"><?php print_r($data['details']->username); ?></td>
+                    </tr>
+                    <tr class="necessity-data">
+                        <th width="30%">Organization Email</th>
+                        <td width="70%"><?php print_r($data['details']->email); ?></td>
+                    </tr>
+                    <tr class="necessity-data">
+                        <th width="30%">Organization Name</th>
+                        <td width="70%"><?php print_r($data['details']->orgName); ?></td>
+                    </tr>
+                    <tr class="necessity-data">
+                        <th width="30%">Organization Number</th>
+                        <td width="70%"><?php print_r($data['details']->orgNumber); ?></td>
+                    </tr>
+                    <tr class="necessity-data">
+                        <th width="30%">Organization Type</th>
+                        <td width="70%"><?php print_r($data['details']->orgType); ?></td>
+                    </tr>
+                    <tr class="necessity-data">
+                        <th width="30%">Bank Account Number</th>
+                        <td width="70%"><?php print_r($data['details']->accNumber); ?></td>
+                    </tr>
+                    <tr class="necessity-data">
+                        <th width="30%">Account Holder's Name</th>
+                        <td width="70%"><?php print_r($data['details']->accountHoldersName); ?></td>
+                    </tr>
+                    <tr class="necessity-data">
+                        <th width="30%">Bank Name</th>
+                        <td width="70%"><?php print_r($data['details']->bankName); ?></td>
+                    </tr>
+                    <tr class="necessity-data">
+                        <th width="30%">Branch Name</th>
+                        <td width="70%"><?php print_r($data['details']->branchName); ?></td>
+                    </tr>
+                    <tr class="necessity-data">
+                        <th width="30%">Address</th>
+                        <td width="70%"><?php print_r($data['details']->address); ?></td>
+                    </tr>
+                    <tr class="necessity-data">
+                        <th width="30%">School/ University Letter</th>
+                        <td width="70%">
+                            <img src="<?php echo URLROOT ?>/nic/<?php print_r($data['details']->letterImage); ?>" class="user-img" alt="">
+                        </td>
+                    </tr>
+                    <tr class="necessity-data">
+                        <th width="30%">Phone Number</th>
+                        <td width="70%"><?php print_r($data['details']->phoneNumber); ?></td>
+                    </tr>
+                </table>
+            </div>
+
+        </div>
+    </section>
+</main>
+
+<?php require APPROOT.'/views/inc/footer.php'; ?>
