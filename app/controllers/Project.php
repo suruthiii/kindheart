a351469fc8a0359f62cea02569b45b5ca5b5b510
sakunika <muthunihansani052@gmail.com>
2024-04-22 12:20:55 +0530

    add project controller completed

--- app/controllers/Project.php
+++ app/controllers/Project.php
@@ -20,13 +20,109 @@ class Project extends Controller {
     }
 
     public function addprojects(){
-        $data = [
-            'title' => 'Home Page',
-            'pendingtablerow' => $this->projectModel->getaddedongoingprojects(),
-            'completetablerow' => $this->projectModel->getaddedcompletedprojects()
-        ];
-        $this->view('organization/project/addprojects', $data);
-}
+        if($_SESSION['user_type'] != 'organization') {
+            redirect('pages/404');
+        }else{
+            if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+                $data = [
+                    'projectTitle' => trim($_POST['projectTitle']),
+                    'projectsmilestones' => $_POST['projectsmilestones'],
+                    'milestonebudget' => $_POST['milestonebudget'],
+                    'milestonedescription' => $_POST['milestonedescription'],
+                    'projectTitle_err' => '',
+                    'MilestoneInputblock_err' => ''
+
+                ];
+
+                //Handle file uploads
+                if(isset($_FILES['firstprojectImages']) && isset($_FILES['seconprojectImages'])){
+                    // Define arrays to holds image paths
+                    $firstprojectImagesPath = [];
+                    $seconprojectImagesPath = [];
+
+                    // Process first project milestone uploaded images
+                    foreach($_FILES['firstprojectImages']['tmp_name'] as $key => $tmp_name){
+                        // Check for upload errors
+                        if($_FILES['firstprojectImages']['error'][$key] === UPLOAD_ERR_OK) {
+                            $firstImagesPath = PUBLICROOT.'/projectmilestoneuploadedimages/'.$_FILES['firstprojectImages']['name'][$key];
+                            move_uploaded_file($tmp_name, $firstImagesPath);
+                            $firstprojectImagesPath[] = $firstImagesPath;
+                        } else {
+                            // Handle upload error
+                            $data['upload_error'] = "Error uploading first project images";
+                        }
+                    }
+
+                    // Process second project milestone uploaded images
+                    foreach($_FILES['seconprojectImages']['tmp_name'] as $key => $tmp_name){
+                        // Check for upload errors
+                        if($_FILES['seconprojectImages']['error'][$key] === UPLOAD_ERR_OK) {
+                            $secondImagePath  = PUBLICROOT.'/projectmilestoneuploadedimages/'.$_FILES['seconprojectImages']['name'][$key];
+                            move_uploaded_file($tmp_name, $secondImagePath);
+                            $seconprojectImagesPath[] = $secondImagePath;
+                        } else {
+                            // Handle upload error
+                            $data['upload_error'] = "Error uploading second project images";
+                        }
+                    }
+
+                    // Add image paths to $data array
+                    $data['firstprojectImagesPath'] = $firstprojectImagesPath;
+                    $data['seconprojectImagesPath'] = $seconprojectImagesPath;
+                }
+
+                // check project tile field is empty or not
+                if(empty($data['projectTitle'])){
+                    $data['projectTitle_err']='Please enter the title for the project';
+                }
+
+                // Check for duplicate file paths between first and second project images
+                foreach ($firstprojectImagesPath as $firstImagePath) {
+                    foreach ($seconprojectImagesPath as $secondImagePath) {
+                        if ($firstImagePath === $secondImagePath) {
+                            $data['MilestoneInputblock_err']= "Duplicate file path detected: '{$firstImagePath}'";
+                        }
+                    }
+                }
+
+                //check whether there any errors
+                if(empty($data['MilestoneInputblock_err']) && empty($data['projectTitle_err']) && !empty($data['projectsmilestones']) && !empty($data['milestonebudget']) && !empty($data['milestonedescription']) && !empty($data['firstprojectImagesPath']) && !empty($data['seconprojectImagesPath'])){
+                    if($this->projectModel->addprojectstodb($data)){
+                        redirect('project/postedprojects');
+                    }else{
+                        error_log('Error: Failed to insert data into the database.');
+                        die('something went wrong');
+                    }
+                }else{
+
+                    if ($_SESSION['user_type'] == 'organization') {
+                        $this->view('organization/project/addprojects', $data);
+                    }else {
+                        die('User Type Not Found');
+                    }
+                    
+                }
+                
+            }else{
+
+                $data = [
+                    'projectTitle' => '',
+                    'projectsmilestones' => [],
+                    'milestonebudget' => [],
+                    'milestonedescription' => [],
+                    'firstprojectImagesPath' => [],
+                    'seconprojectImagesPath' => [],
+                    'projectTitle_err' => '',
+                    'MilestoneInputblock_err' => ''
+                ];
+
+                $this->view('organization/project/addprojects', $data);
+
+            }
+        }
+ 
+    }
 
     public function manageProject($project_ID = null) {
         if(($_SESSION['user_type'] != 'admin' && $_SESSION['user_type'] != 'superAdmin') ||  empty($_GET['project_ID']) && empty($_POST['project_ID'])) {
