65e596689531ef2ef2e863126f44900b739f11fe
E.G.D.Dulaji <dithuegodagamage@gmail.com>
2024-04-24 04:25:26 +0530

    donor dashboard front end

--- app/views/donor/index.php
+++ app/views/donor/index.php
@@ -8,43 +8,68 @@
 
 <main class="page-container">
     <section class="section" id="main">
-        <div class="donor-right-side-container">
+        <div class="donor-dashboard-container">
 
             <!-- Title container -->
-            <div class="donor-div-one">
+            <div class="donor-dashboard-introduction">
                 <h1>Welcome Back <b>....Name.....<b></h1>
                 <p>Here's The Latest Updates</p>
             </div>
 
             <!-- Status container -->
-            <div class="donor-div-two">
-                <h4>Status</h4>
-                <div class="status">
-                    <p>Donation Status</p>
+            <div class="donor-dashboard-status-bar-container">
+                <div class="donor-dashboard-status-topic">
+                    <h4>Status</h4>
                 </div>
-                <div class="status">
-                    <p>Donation Status</p>
-                </div>
-                <div class="status">
-                    <p>Donation Status</p>
-                </div>
-                <div class="status">
-                    <p>Donation Status</p>
+                <div class="donor-dashboard-status-bar">
+                    <div class="donor-dashboard-status">
+                        <p>Donation Status</p>
+                    </div>
+                    <div class="donor-dashboard-status">
+                        <p>Donation Status</p>
+                    </div>
+                    <div class="donor-dashboard-status">
+                        <p>Donation Status</p>
+                    </div>
+                    <div class="donor-dashboard-status">
+                        <p>Donation Status</p>
+                    </div>
+                    <div class="donor-dashboard-status">
+                        <p>Donation Status</p>
+                    </div>
                 </div>
             </div>
 
-            <!-- Status container -->
-            <div class="donor-div-three">
-                <!-- Graph container -->
-                <div class="donor-right-container">
-                    <h4>Donation Summary</h4>
-                    <p>----------------</p>
+            <div class="donor-dashboard-two-column-container">
+                <div class="donor-dashboard-graph-container">
+                    <!-- Graph container -->
+                    <div class="donor-dashboard-status-topic">
+                        <h4>Donation Summary</h4>
+                    </div>
+                    <div class="donor-dashboard-left-container">
+                        <p>----------------</p>
+                    </div>
                 </div>
                 
-                <!-- Donation container -->
-                <div class="donor-left-container">
-                    <h4>Recent Donations</h4>
-                    <p>------------</p>
+                <div class="donor-dashboard-donation-container">
+                    <!-- Donation container -->
+                    <div class="donor-dashboard-status-topic">
+                        <h4>Recent Donations</h4>
+                    </div>
+                    <div class="donor-dashboard-right-container">
+                        <table>
+                            <tr>
+                                <th>Donation</th>
+                                <th>Date</th>
+                                <th>Status</th>
+                            </tr>
+                            <tr>
+                                <td>Donation</td>
+                                <td>Date</td>
+                                <td>Status</td>
+                            </tr>
+                        </table>
+                    </div>
                 </div>  
             </div>                   
         </div>
