3f051a25c7147ff43da523f422a0fab3abaa3977
sakunika <muthunihansani052@gmail.com>
2024-04-26 09:42:34 +0530

    Edit one-time necessities completed from view page

--- app/views/organization/necessity/viewOrganizationPendingMonetarynecessity.php
+++ app/views/organization/necessity/viewOrganizationPendingMonetarynecessity.php
@@ -77,7 +77,7 @@
                         </button>
                     </form>
                 <?php } else if($data['pendingNecessityDetails']->monetaryNecessityType == "onetime"){ ?>
-                    <form action="<?php echo URLROOT ?>" method="post">
+                    <form action="<?php echo URLROOT ?>/necessity/editOnetimeMonetaryNecessity" method="post">
                         <input type="hidden" name="necessityID" id="necessityID" value="<?php echo $data['pendingNecessityDetails']->necessityID ; ?>" />
                         <button type="submit">
                             <img src="<?php echo URLROOT ?>/img/pen-to-square-solid.svg" class="ncessity-view-table-edit-button-img">
