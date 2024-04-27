8165f24fce90b9634efdd75b29adcecd18f9f30a
jayathmaa <78878373+jayathmaa@users.noreply.github.com>
2024-04-27 23:40:36 +0530

    edit profile completed

--- app/views/student/editProfile.php
+++ app/views/student/editProfile.php
@@ -42,43 +42,73 @@
                             <label for="">User Name</label>
                             <div class="form-field">
                                 <div class="form-field-data">
-                                    <input type="text" value="<?php print_r($_SESSION['user_name']) ?>" name="fName" readonly required>
+                                    <input type="text" value="<?php print_r($_SESSION['user_name']) ?>" name="username" readonly required>
                                 </div>
                             </div>
                     </div>
                  
                         <div class="lines">
                             <label for="">First Name</label>
-                            <input type="text" value="<?php print_r($data['studentData']->fName) ?>" name="fName" readonly required>
+                            <div class="form-field">
+                                <div class="form-field-data">
+                                    <input type="text" value="<?php print_r($data['studentData']->fName) ?>" name="fName" readonly required>
+                                </div>
+                            </div>
                         </div>
                    
                         <div class="lines">
                             <label for="">Last Name</label>
-                            <input type="text" value="<?php print_r($data['studentData']->lName) ?>" name="lName" readonly required>  
+                            <div class="form-field">
+                                <div class="form-field-data">
+                                    <input type="text" value="<?php print_r($data['studentData']->lName) ?>" name="lName" readonly required>
+                                </div>
+                            </div>  
                         </div>
                         <div class="lines">
                             <label for="">Phone Number</label>
-                            <input type="text" value="<?php print_r($data['studentData']->phoneNumber) ?>" name="phoneNumber" required>  
+                            <div class="form-field">
+                                <div class="form-field-data">
+                                    <input type="text" value="<?php print_r($data['studentData']->phoneNumber) ?>" name="phoneNumber" readonly required>
+                                </div>
+                            </div>    
                         </div>
 
                         <div class="lines">
                             <label for="">Gender</label>
-                            <input type="text" value="<?php print_r($data['studentData']->gender) ?>" name="gender" readonly required>  
+                            <div class="form-field">
+                                <div class="form-field-data">
+                                    <input type="text" value="<?php print_r($data['studentData']->gender) ?>" name="gender" readonly required>
+                                </div>
+                            </div>   
                         </div>
                
                         <div class="lines">
                             <label for="">Address</label>
-                            <input type="textarea" value="<?php print_r($data['studentData']->address) ?>" name="address" required></textarea>
+                            <div class="form-field">
+                                <div class="form-field-data">
+                                    <input type="text" value="<?php print_r($data['studentData']->address) ?>" name="address" readonly required>
+                                </div>
+                            </div>   
+                            
                         </div>
            
                         <div class="lines">
                             <label for="">Date Of Birth</label>
-                            <input type="tel" value="<?php print_r($data['studentData']->dateOfBirth)?>" name="dateOfBirth" readonly required>  
+                            <div class="form-field">
+                                <div class="form-field-data">
+                                    <input type="text" value="<?php print_r($data['studentData']->dateOfBirth)?>" name="dateOfBirth" readonly required>
+                                </div>
+                            </div>   
+        
                         </div>
                   
                         <div class="lines">
                             <label for="">National Identity Card Number</label>
-                            <input type="text" value="<?php print_r($data['studentData']->nicNumber) ?>" name="nicNumber" readonly required>
+                            <div class="form-field">
+                                <div class="form-field-data">
+                                    <input type="text" value="<?php print_r($data['studentData']->nicNumber) ?>" name="nicNumber" readonly required>
+                                </div>
+                            </div>   
                         </div>
 
                         <div class="lines">
@@ -103,58 +133,167 @@
 
                         <div class="lines">
                             <label for="">Year Of Studying</label>
-                            <input type="text" value="<?php print_r($data['studentData']->studyingYear) ?>" name="studyingYear" required>
+                            <div class="form-field">
+                                <div class="form-field-data">
+                                    <input type="text" value="<?php print_r($data['studentData']->studyingYear) ?>" name="studyingYear" readonly required>
+                                </div>
+
+                                <div class="form-field-visibility">
+                                    <label class="switch">
+                                    <?php if ($data['studentData']->institutionNameVisibility==1): ?>
+                                        <input type="checkbox" name="studyingYearVisibility" checked>
+                                    <?php else: ?>
+                                        <input type="checkbox" name="studyingYearVisibility" >
+                                    <?php endif; ?>
+                                        <span class="slider round"></span>
+                                    </label>
+                                </div>
+                            </div>
                         </div>
 
                         <div class="lines">
                             <label for="">Student Type</label>
-                            <input type="text" value="<?php print_r($data['studentData']->studentType) ?>" name="studentType" required>  
+                            <div class="form-field">
+                                <div class="form-field-data">
+                                    <input type="text" value="<?php print_r($data['studentData']->studentType) ?>" name="studentType"readonly required>
+                                </div>
+                            </div>   
+                             
                         </div>
 
                         <div class="lines">
                             <label for="">Caregiver Name</label>
-                            <input type="text" value="<?php print_r($data['studentData']->caregiverName) ?>" name="caregiverName" required>  
+                            <div class="form-field">
+                                <div class="form-field-data">
+                                    <input type="text" value="<?php print_r($data['studentData']->caregiverName) ?>" name="caregiverName" readonly required>
+                                </div>
+
+                                <div class="form-field-visibility">
+                                    <label class="switch">
+                                    <?php if ($data['studentData']->caregiverNameVisibility==1): ?>
+                                        <input type="checkbox" name="caregiverNameVisibility" checked>
+                                    <?php else: ?>
+                                        <input type="checkbox" name="caregiverNameVisibility" >
+                                    <?php endif; ?>
+                                        <span class="slider round"></span>
+                                    </label>
+                                </div>
+                            </div>
+                            
                         </div>
 
                         <div class="lines">
                             <label for="" >Caregiver Type</label>
-                            <input type="text" value="<?php print_r($data['studentData']->caregiverType) ?>" name="caregiverType" required>  
+                            <div class="form-field">
+                                <div class="form-field-data">
+                                    <input type="text" value="<?php print_r($data['studentData']->caregiverType) ?>" name="caregiverType" readonly required>
+                                </div>
+
+                                <div class="form-field-visibility">
+                                    <label class="switch">
+                                    <?php if ($data['studentData']->caregiverTypeVisibility==1): ?>
+                                        <input type="checkbox" name="caregiverTypeVisibility" checked>
+                                    <?php else: ?>
+                                        <input type="checkbox" name="caregiverTypeVisibility" >
+                                    <?php endif; ?>
+                                        <span class="slider round"></span>
+                                    </label>
+                                </div>
+                            </div>
+                             
                         </div>
                         <div class="lines">
                             <label for="">Caregiver's relation to the student</label>
-                            <input type="text" value="<?php print_r($data['studentData']->caregiverRelationship) ?>" name="caregiverRelationship" required>  
+                            <div class="form-field">
+                                <div class="form-field-data">
+                                    <input type="text" value="<?php print_r($data['studentData']->caregiverRelationship) ?>" name="caregiverRelationship" readonly required>
+                                </div>
+
+                                <div class="form-field-visibility">
+                                    <label class="switch">
+                                    <?php if ($data['studentData']->caregiverRelationshipVisibility==1): ?>
+                                        <input type="checkbox" name="caregiverRelationshipVisibility" checked>
+                                    <?php else: ?>
+                                        <input type="checkbox" name="caregiverRelationshipVisibility" >
+                                    <?php endif; ?>
+                                        <span class="slider round"></span>
+                                    </label>
+                                </div>
+                            </div>
+                           
                         </div>
 
                         <div class="lines">
                             <label for="">Caregiver's Occupation</label>
-                            <input type="text" value="<?php print_r($data['studentData']->caregiverOccupation) ?>" name="caregiverOccupation" >  
+                            <div class="form-field">
+                                <div class="form-field-data">
+                                    <input type="text" value="<?php print_r($data['studentData']->caregiverOccupation) ?>" name="caregiverOccupation"  readonly required>
+                                </div>
+
+                                <div class="form-field-visibility">
+                                    <label class="switch">
+                                    <?php if ($data['studentData']->caregiverOccupationVisibility==1): ?>
+                                        <input type="checkbox" name="caregiverOccupationVisibility" checked>
+                                    <?php else: ?>
+                                        <input type="checkbox" name="caregiverOccupationVisibility" >
+                                    <?php endif; ?>
+                                        <span class="slider round"></span>
+                                    </label>
+                                </div>
+                            </div>
+                           
                         </div>
 
                         <div class="lines">
                             <label for="">Currently Receiving Scholarships</label>
-                            <input type="textarea" value="<?php print_r($data['studentData']->receivingScholarships) ?>" name="caregiverOccupation" >  
+                            <div class="form-field">
+                                <div class="form-field-data">
+                                    <input type="text" value="<?php print_r($data['studentData']->receivingScholarships) ?>" name="receivingScholarships" >
+                                </div>
+                            </div>   
+                            
                         </div>
 
                         <h4> Bank Details</h4>
 
                         <div class="lines">
                             <label for="">Account Holder's Name</label>
-                            <input type="text" value="<?php print_r($data['studentData']->accountHoldersName) ?>" name="accountHoldersName"  required>  
+                            <div class="form-field">
+                                <div class="form-field-data">
+                                    <input type="text" value="<?php print_r($data['studentData']->accountHoldersName) ?>" name="accountHoldersName" required>
+                                </div>
+                            </div>   
+                           
                         </div>
 
                         <div class="lines">
                             <label for="">Account Number</label>
-                            <input type="text" value="<?php print_r($data['studentData']->accNumber) ?>" name="accNumber" required>  
+                            <div class="form-field">
+                                <div class="form-field-data">
+                                    <input type="text" value="<?php print_r($data['studentData']->accNumber) ?>" name="accNumber" required>
+                                </div>
+                            </div>   
+                 
                         </div>
 
                         <div class="lines">
                             <label for="">Bank</label>
-                            <input type="text" value="<?php print_r($data['studentData']->bankName) ?>" name="bankName" required>  
+                            <div class="form-field">
+                                <div class="form-field-data">
+                                    <input type="text" value="<?php print_r($data['studentData']->bankName) ?>" name="bankName" required>
+                                </div>
+                            </div>  
+                            
                         </div>
 
                         <div class="lines">
                             <label for="">Branch</label>
-                            <input type="text" value="<?php print_r($data['studentData']->branchName) ?>" name="branchName" required>  
+                            <div class="form-field">
+                                <div class="form-field-data">
+                                    <input type="text" value="<?php print_r($data['studentData']->branchName) ?>" name="branchName" required>
+                                </div>
+                            </div>  
+                           
                         </div>
                         
                         <!-- Add button -->
